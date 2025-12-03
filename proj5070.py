42bd138bdc888eadb6d5eb57d1c386d1f97b7a8a
C. Mikolaitis <133790086+CMikolaitis@users.noreply.github.com>
2025-12-03 08:59:13 -0600

    Update proj5070.py

--- proj5070.py
+++ proj5070.py
@@ -36,6 +36,12 @@ with Dataset(infile, "r") as src, Dataset(outfile, "w") as dst:
 
     lon_var = dst.createVariable("lon", "f8", src.variables["lon"].dimensions, zlib=True)
     lat_var = dst.createVariable("lat", "f8", src.variables["lat"].dimensions, zlib=True)
+
+    # Add projection metadata
+    lon_var.long_name = "x coordinate in EPSG:5070 (NAD83 / Conus Albers)"
+    lon_var.units = "meters"
+    lat_var.long_name = "y coordinate in EPSG:5070 (NAD83 / Conus Albers)"
+    lat_var.units = "meters"
     
     lon_var[:] = x
     lat_var[:] = y
