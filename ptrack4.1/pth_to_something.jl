0b8a25ff51d91dc314ffb6fbb7779482ef9eafd5
C. Mikolaitis <133790086+CMikolaitis@users.noreply.github.com>
2026-01-08 13:06:23 -0600

    Update pth_to_something.jl

--- ptrack4.1/pth_to_something.jl
+++ ptrack4.1/pth_to_something.jl
@@ -1,21 +1,34 @@
+function subdomain_gr3(infile)
+	lines = readlines(filename)
+    # Second line has ne np
+    ne, np = parse.(Int, split(lines[2]))
+    # Element lines start after the nodes
+    ele_lines = lines[2 + np + 1 : 2 + np + ne]
+    # Extract element IDs
+    ele = [parse(Int, split(line)[1]) for line in ele_lines]
+    return ele
+
 open(pth_file,"r") do io
         while !eof(io)
+			# Handle header
             header = readline(io)
             parts  = split(strip(header))
             length(parts)!=2 && continue
             time_sec, n_particles = try parse(Float64, parts[1]), parse(Int, parts[2]) catch; continue end
+			# Initialize empty vectors for number of particles
             pidv   = Vector{Int}(undef,n_particles)
-	          elev   = Vector{Int}(undef,n_particles)
+	        elev   = Vector{Int}(undef,n_particles)
             lonv   = Vector{Float64}(undef,n_particles)
             latv   = Vector{Float64}(undef,n_particles)
             depthv = Vector{Float64}(undef,n_particles)
             timev  = Vector{Float64}(undef,n_particles)
+			# Fill
             for i in 1:n_particles
                 line=readline(io)
                 vals=split(strip(line))
                 length(vals)<5 && error("Malformed line: $line")
                 pidv[i]   = parse(Int,vals[1])
-		            elev[i]   = parse(Int,vals[2])
+		        elev[i]   = parse(Int,vals[2])
                 lonv[i]   = parse(Float64,vals[3])
                 latv[i]   = parse(Float64,vals[4])
                 depthv[i] = parse(Float64,vals[5])
